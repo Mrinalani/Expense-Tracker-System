11c6d2f46c486e52f358055eab5f7fbf2055887c
pandeymrinalani <pandeymrinalani02@gmail.com>
2023-10-28 20:10:20 +0530

    Deployment

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ app.use((req, res, next) => {
 app.use((req, res) => {
     console.log('urlll', req.url);
     //res.sendFile(path.join(__dirname, 'public/login/login.html'));
+    console.log("random url")
    res.sendFile(path.join(__dirname, `public/${req.url}`));
 
 });
